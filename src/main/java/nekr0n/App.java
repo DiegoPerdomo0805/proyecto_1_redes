71e7d4eff79485334a2cc2e5c5aadb4716abd6d6
DiegoP0805 <per20204@uvg.edu.gt>
2023-08-22 15:18:45 -0600

    sign up and delete

--- src/main/java/nekr0n/App.java
+++ src/main/java/nekr0n/App.java
@@ -29,24 +29,25 @@ public class App
             if (m.option(op, 0, options.length))
             {
                 option = Integer.parseInt(op);
+
+                // declarar variables que se usan en todos los casos
+                Comm comm = new Comm(Conn.getConnection());
+                String user;
+                String psswrd;
+                String who;
+                String what;
+                // acciones disponibles
+                String[] actions = {                            "Mostrar online y estados",                            "Mostrar contacto",                            "Agregar contacto",                            "Enviar mensaje directo",                            "Enviar mensaje grupal",                            "Cambiar estado",                            "Enviar archivo",                            "Cerrar sesión",                            "Eliminar cuenta"                        };
                 switch (option)
                 {
                     case 1:
                         System.out.println("Registrarse");
-                        // register
-                        // igaul que con login
-                        break;
-                    case 2:
                         System.out.println("Ingrese su nombre de usuario: ");
-                        String user = sc.nextLine();
+                        user = sc.nextLine();
                         System.out.println("Ingrese su contraseña: ");
-                        String psswrd = sc.nextLine();
-                        Comm comm = new Comm(Conn.getConnection());
-                        Conn.logIn(comm.conn, user, psswrd);
-                        
+                        psswrd = sc.nextLine();
+                        Conn.signUp(comm.conn, user, psswrd);
 
-                        String[] actions = {
-                            "\nMostrar online y estados",                            "Mostrar contacto",                            "Agregar contacto",                            "Enviar mensaje directo",                            "Enviar mensaje grupal",                            "Cambiar estado",                            "Enviar archivo",                            "Cerrar sesión",                            "Eliminar cuenta"                        };
                         while (action < 8)
                         {
                             System.out.println("\n");
@@ -55,8 +56,8 @@ public class App
                             if (m.option(act, 0, actions.length))
                             {
                                 action = Integer.parseInt(act);
-                                String who = "";
-                                String what = "";
+                                who = "";
+                                what = "";
 
                                 switch (action)
                                 {
@@ -82,6 +83,7 @@ public class App
                                         comm.listener();
                                         while (!what.equals("-exit"))
                                         {
+                                            // System.out.print(comm.conn.getUser().asBareJid().toString() + ": ");
                                             what = sc.nextLine();
                                             if (!what.equals("-exit"))
                                             {
@@ -115,7 +117,97 @@ public class App
                                         break;
                                     case 9:
                                         System.out.println("Eliminar cuenta");
-                                        // delete
+                                        Conn.deleteAccount(comm.conn);
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                System.out.println("Opción no válida");
+                            }
+                        }
+
+
+                        // igaul que con login
+                        break;
+                    case 2:
+                        System.out.println("Ingrese su nombre de usuario: ");
+                        user = sc.nextLine();
+                        System.out.println("Ingrese su contraseña: ");
+                        psswrd = sc.nextLine();
+                        Conn.logIn(comm.conn, user, psswrd);
+
+                        
+                        while (action < 8)
+                        {
+                            System.out.println("\n");
+                            m.Options(actions);
+                            String act = sc.nextLine();
+                            if (m.option(act, 0, actions.length))
+                            {
+                                action = Integer.parseInt(act);
+                                who = "";
+                                what = "";
+
+                                switch (action)
+                                {
+                                    case 1:
+                                        // ping
+                                        break;
+                                    case 2:
+                                        System.out.println("Ingrese el nombre de usuario a buscar: ");
+                                        who = sc.nextLine();
+                                        System.out.println(who);
+                                        // lookup
+                                        break;
+                                    case 3:
+                                        System.out.println("Ingrese el nombre de usuario a agregar: ");
+                                        who = sc.nextLine();
+                                        System.out.println(who);
+                                        // add
+                                        break;
+                                    case 4:
+                                        System.out.println("Escriba el destinatario: ");
+                                        who = sc.nextLine();                                    
+                                        System.out.println(" - Para salir del chat, escriba -exit y presione enter");
+                                        comm.listener();
+                                        while (!what.equals("-exit"))
+                                        {
+                                            //System.out.print(comm.conn.getUser().asBareJid().toString() + ": ");
+                                            what = sc.nextLine();
+                                            if (!what.equals("-exit"))
+                                            {
+                                                comm.dm(who, what);
+                                            }
+                                        }
+                                        comm.stopListener();
+                                        // dm
+                                        break;
+                                    case 5:
+                                        System.out.println("Escriba los destinatarios separados por comas: ");
+                                        who = sc.nextLine();
+                                        System.out.println("Escriba su mensaje:");
+                                        what = sc.nextLine();
+                                        System.out.println(what + " " + who);
+                                        // gm
+                                        break;
+                                    case 6:
+                                        System.out.println("Ingrese su nuevo estado: ");
+                                        what = sc.nextLine();
+                                        System.out.println(what);
+                                        // status
+                                        break;
+                                    case 7:
+                                        System.out.println("Enviar archivo");
+                                        // attach
+                                        break;
+                                    case 8:
+                                        System.out.println("Cerrando sesión...");
+                                        Conn.logOff(comm.conn);
+                                        break;
+                                    case 9:
+                                        System.out.println("Eliminar cuenta");
+                                        Conn.deleteAccount(comm.conn);
                                         break;
                                 }
                             }
